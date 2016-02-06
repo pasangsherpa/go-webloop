ee6c8e0b1acaa849f4ebe088b2a945567cdbfc32
Pasang Sherpa <pasang_sh@hotmail.com>
2016-02-05 19:26:23 -0800

    xvfb display value updated

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-DISPLAY=:1
+DISPLAY=:0.0
 XVFB=/usr/bin/Xvfb
 XVFBARGS="$DISPLAY -ac -screen 0 1024x768x16 +extension RANDR"
 PIDFILE="/var/xvfb.pid"
 
-export DISPLAY=$DISPLAY
+echo "export DISPLAY=$DISPLAY" >> $HOME/.bashrc
 
 /sbin/start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
 sleep 1
